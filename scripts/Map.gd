cb28e4346879c006e60ff6280913f727430f788d
NickLatkovich <n.latkovich@pixelplex.by>
2018-08-12 12:07:11 +0300

    Change stars' size

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -4,7 +4,9 @@ var matrix
 func _draw():
 	for y in range(matrix.size()):
 		for x in range(matrix[y].size()):
-			var rect = Rect2(Vector2(x, y) * 64, Vector2(64, 64))
 			match matrix[y][x]:
-				1: draw_rect(rect, Color(0, 0, 0))
-				2: draw_texture_rect(star_texture, rect, false)
+				1: draw_rect(Rect2(Vector2(x, y) * 64, Vector2(64, 64)), Color(0, 0, 0))
+				2: draw_texture_rect(
+					star_texture,
+					Rect2(Vector2(x, y) * 64 + Vector2(16, 16), Vector2(32, 32)),
+					false)
