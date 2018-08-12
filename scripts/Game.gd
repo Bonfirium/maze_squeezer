b20265553a77e9fcc81804174113a66609cb7e92
NickLatkovich <n.latkovich@pixelplex.by>
2018-08-12 11:08:37 +0300

    Done maze-generator

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -3,13 +3,19 @@ export (int) var width = 16
 export (int) var height = 16
 var matrix = []
 var player
+var dirs = [
+	Vector2(1, 0),
+	Vector2(0, -1),
+	Vector2(-1, 0),
+	Vector2(0, 1),
+]
 func save_matrix(temp_matrix):
 	var result = []
 	for y in range(height):
 		var column = []
 		column.resize(width)
 		for x in range(width):
-			column[x] = temp_matrix[y][x] == 3
+			column[x] = temp_matrix[y][x] != 2
 		result.append(column)
 	return result
 func generate_maze():
@@ -18,12 +24,28 @@ func generate_maze():
 	for y in range(height):
 		var column = []
 		column.resize(width)
-		for x in range(width): column[x] = 0 if y > 0 and y < pre_size.y and x > 0 and x < pre_size.x else 3
+		for x in range(width):
+			column[x] = 0 if y > 0 and y < pre_size.y and x > 0 and x < pre_size.x else 3
 		temp_matrix.append(column)
+	var q = [Vector2(1 + randi() % (width - 2), 1 + randi() % (height - 2))]
+	temp_matrix[q[0].y][q[0].x] = 1
+	while q.size() > 0:
+		var index = randi() % q.size()
+		var pos = q[index]
+		if (index != q.size() - 1): q[index] = q.pop_back()
+		else: q.pop_back()
+		if temp_matrix[pos.y][pos.x] != 1: continue
+		temp_matrix[pos.y][pos.x] = 2
+		for dir in dirs:
+			var this_pos = pos + dir
+			match temp_matrix[this_pos.y][this_pos.x]:
+				1: temp_matrix[this_pos.y][this_pos.x] = 3
+				0:
+					temp_matrix[this_pos.y][this_pos.x] = 1
+					q.append(this_pos)
 	return save_matrix(temp_matrix)
 func _ready():
 	matrix = generate_maze()
-	print(matrix)
 	player = $Player
 	player.position = Vector2(randi() % width + 0.5, randi() % height + 0.5) * 64
 	player.matrix = matrix
